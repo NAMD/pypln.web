2f2c17da6086b733726ffd5b0f93021a88a1550f
Flavio Amieiro <amieiro.flavio@gmail.com>
2012-12-18 13:09:00 -0200

    Add first corpus page test

--- pypln/web/apps/core/tests.py
+++ pypln/web/apps/core/tests.py
@@ -6,11 +6,14 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class CorpusViewTest(TestCase):
+    def test_requires_login(self):
+        response = self.client.get(reverse('corpus_page',
+            kwargs={'corpus_slug': 'test-corpus'}))
+        self.assertEqual(response.status_code, 302)
+        login_url = settings.LOGIN_URL
+        self.assertTrue(login_url in response['Location'])
