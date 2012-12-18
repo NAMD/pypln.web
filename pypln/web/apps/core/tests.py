b00abd493f6f594c8ec94e9cf90454be8dd3613b
Flavio Amieiro <amieiro.flavio@gmail.com>
2012-12-18 13:31:35 -0200

    Adds tests for successful get on corpus page

--- pypln/web/apps/core/tests.py
+++ pypln/web/apps/core/tests.py
@@ -9,6 +9,8 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
+from core.models import Corpus, DocumentForm
+
 
 class CorpusViewTest(TestCase):
     fixtures = ['corpus']
@@ -24,3 +26,30 @@ class CorpusViewTest(TestCase):
         response = self.client.get(reverse('corpus_page',
             kwargs={'corpus_slug': 'corpus-that-isnt-there'}))
         self.assertEqual(response.status_code, 404)
+
+    def test_shows_existing_corpus_without_error(self):
+        self.client.login(username="admin", password="admin")
+        response = self.client.get(reverse('corpus_page',
+            kwargs={'corpus_slug': 'test-corpus'}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "core/corpus.html")
+        self.assertNotIn(settings.TEMPLATE_STRING_IF_INVALID, response.content)
+
+    def test_corpus_is_in_context(self):
+        self.client.login(username="admin", password="admin")
+        response = self.client.get(reverse('corpus_page',
+            kwargs={'corpus_slug': 'test-corpus'}))
+
+        corpus = Corpus.objects.get(slug="test-corpus")
+
+        self.assertIn("corpus", response.context)
+        self.assertEqual(response.context["corpus"], corpus)
+
+    def test_document_upload_form_is_in_context(self):
+        self.client.login(username="admin", password="admin")
+        response = self.client.get(reverse('corpus_page',
+            kwargs={'corpus_slug': 'test-corpus'}))
+
+        self.assertIn("form", response.context)
+        self.assertIsInstance(response.context["form"], DocumentForm)
