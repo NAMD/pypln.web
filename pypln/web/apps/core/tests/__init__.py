b8db7f14975a06d66d66a0ed61539750adeec47a
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-01-07 20:31:16 -0200

    Moves model tests to their own file

--- pypln/web/apps/core/tests/__init__.py
+++ pypln/web/apps/core/tests/__init__.py
@@ -50,50 +50,7 @@ from core import views
 
 from views import *
 from forms import *
-
-class DocumentModelTest(TestCase):
-    fixtures = ['corpus']
-
-    def setUp(self):
-        if 'test' not in gridfs_storage.database:
-            error_message = ("We expect the mongodb database name to contain the "
-                "string 'test' to make sure you don't mess up your production "
-                "database. Are you sure you're using settings.test to run these "
-                "tests?")
-            raise ImproperlyConfigured(error_message)
-
-        gridfs_storage._connection.drop_database(gridfs_storage.database)
-
-        self.user = User.objects.all()[0]
-        self.file = ContentFile("Bring us a shrubbery!")
-        self.file.name = "42.txt"
-
-    def tearDown(self):
-        gridfs_storage._connection.drop_database(gridfs_storage.database)
-
-    def test_saving_should_generate_a_slug(self):
-        doc = Document(owner=self.user, blob=self.file)
-        doc.save()
-        self.assertEqual(doc.slug, "42.txt")
-
-    def test_saving_another_file_with_the_same_name_should_generate_new_slug(self):
-        doc = Document(owner=self.user, blob=self.file)
-        doc.save()
-        self.assertEqual(doc.slug, "42.txt")
-
-        other_file = ContentFile("Bring us another shrubbery!")
-        other_file.name = "42.txt"
-        other_doc = Document(owner=self.user, blob=other_file)
-        other_doc.save()
-        self.assertEqual(other_doc.slug, "42_1.txt")
-
-    def test_editing_a_file_should_not_alter_slug(self):
-        doc = Document(owner=self.user, blob=self.file)
-        doc.save()
-        self.assertEqual(doc.slug, "42.txt")
-        doc.date_uploaded = datetime.now()
-        doc.save()
-        self.assertEqual(doc.slug, "42.txt")
+from models import *
 
 def _create_document(filename, contents, owner):
     fp = StringIO()
---
+++ pypln/web/apps/core/tests/models.py
@@ -0,0 +1,96 @@
+# coding: utf-8
+#
+# Copyright 2012 NAMD-EMAP-FGV
+#
+# This file is part of PyPLN. You can get more information at: http://pypln.org/.
+#
+# PyPLN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyPLN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+
+from StringIO import StringIO
+from datetime import datetime
+from mock import patch
+import shutil
+
+import pymongo
+
+from datetime import datetime
+from StringIO import StringIO
+
+from django.core import management
+from django.core.files import File
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.test.client import RequestFactory
+from django.core.exceptions import ObjectDoesNotExist, ImproperlyConfigured
+from django.core.files.base import ContentFile
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+from django.contrib.auth.models import User
+
+from django.test.client import Client
+from mongodict import MongoDict
+
+from core.models import gridfs_storage, Corpus, Document, index_schema
+from core.forms import DocumentForm
+from core import views
+
+__all__ = ["DocumentModelTest"]
+
+class DocumentModelTest(TestCase):
+    fixtures = ['corpus']
+
+    def setUp(self):
+        if 'test' not in gridfs_storage.database:
+            error_message = ("We expect the mongodb database name to contain the "
+                "string 'test' to make sure you don't mess up your production "
+                "database. Are you sure you're using settings.test to run these "
+                "tests?")
+            raise ImproperlyConfigured(error_message)
+
+        gridfs_storage._connection.drop_database(gridfs_storage.database)
+
+        self.user = User.objects.all()[0]
+        self.file = ContentFile("Bring us a shrubbery!")
+        self.file.name = "42.txt"
+
+    def tearDown(self):
+        gridfs_storage._connection.drop_database(gridfs_storage.database)
+
+    def test_saving_should_generate_a_slug(self):
+        doc = Document(owner=self.user, blob=self.file)
+        doc.save()
+        self.assertEqual(doc.slug, "42.txt")
+
+    def test_saving_another_file_with_the_same_name_should_generate_new_slug(self):
+        doc = Document(owner=self.user, blob=self.file)
+        doc.save()
+        self.assertEqual(doc.slug, "42.txt")
+
+        other_file = ContentFile("Bring us another shrubbery!")
+        other_file.name = "42.txt"
+        other_doc = Document(owner=self.user, blob=other_file)
+        other_doc.save()
+        self.assertEqual(other_doc.slug, "42_1.txt")
+
+    def test_editing_a_file_should_not_alter_slug(self):
+        doc = Document(owner=self.user, blob=self.file)
+        doc.save()
+        self.assertEqual(doc.slug, "42.txt")
+        doc.date_uploaded = datetime.now()
+        doc.save()
+        self.assertEqual(doc.slug, "42.txt")
+
