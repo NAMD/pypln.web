ad96ae163898299e6771fb0c61dddbe5ce3ff8fb
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-01-07 20:29:04 -0200

    Moves Form test to it's own file

--- pypln/web/apps/core/tests/__init__.py
+++ pypln/web/apps/core/tests/__init__.py
@@ -49,90 +49,7 @@ from core.forms import DocumentForm
 from core import views
 
 from views import *
-
-class DocumentFormTest(TestCase):
-    fixtures = ['corpus']
-
-    def setUp(self):
-        self.url = reverse('corpus_page',
-            kwargs={'corpus_slug': 'test-corpus'})
-        self.user = User.objects.all()[0]
-
-        self.fp = StringIO("Bring us a shrubbery!!")
-        self.fp.name = "42.txt"
-
-        self.fp2 = StringIO("Bring us another shrubbery!!")
-        self.fp2.name = "43.txt"
-
-        self.request_factory = RequestFactory()
-
-    def tearDown(self):
-        self.fp.close()
-        self.fp2.close()
-
-    def test_form_is_valid_with_one_file(self):
-        request = self.request_factory.post(self.url, {"blob": self.fp})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        self.assertTrue(form.is_valid())
-
-    def test_at_least_one_file_is_required(self):
-        request = self.request_factory.post(self.url, {"blob": []})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        self.assertFalse(form.is_valid())
-
-    def test_form_is_valid_with_multiple_files(self):
-        request = self.request_factory.post(self.url,
-                                            {"blob": [self.fp, self.fp2]})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        self.assertTrue(form.is_valid())
-
-    def test_form_saves_document_with_correct_user(self):
-        self.assertEqual(len(Document.objects.all()), 0)
-        request = self.request_factory.post(self.url, {"blob": self.fp})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        docs = form.save()
-        self.assertEqual(docs[0].owner, self.user)
-        self.assertEqual(len(Document.objects.all()), 1)
-
-    def test_form_saves_document_with_correct_content(self):
-        self.assertEqual(len(Document.objects.all()), 0)
-        request = self.request_factory.post(self.url, {"blob": self.fp})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        docs = form.save()
-        self.assertEqual(len(Document.objects.all()), 1)
-        doc = Document.objects.all()[0]
-        self.assertEqual(doc.blob.read(), "Bring us a shrubbery!!")
-
-    def test_save_raises_ValueError_if_data_isnt_valid(self):
-        self.assertEqual(len(Document.objects.all()), 0)
-        request = self.request_factory.post(self.url, {"blob": []})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        self.assertRaises(ValueError, form.save)
-
-    def test_form_only_returns_document_if_commit_is_false(self):
-        self.assertEqual(len(Document.objects.all()), 0)
-        request = self.request_factory.post(self.url, {"blob": self.fp})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        docs = form.save(commit=False)
-        self.assertEqual(docs[0].owner, self.user)
-        self.assertEqual(len(Document.objects.all()), 0)
-
-    def test_blob_widget_has_multiple_attr(self):
-        request = self.request_factory.post(self.url, {"blob": self.fp})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        self.assertEqual(form.fields['blob'].widget.attrs['multiple'],
-                         'multiple')
-
-    def test_form_saves_more_than_one_document(self):
-        self.assertEqual(len(Document.objects.all()), 0)
-        request = self.request_factory.post(self.url, {"blob": [self.fp, self.fp2]})
-        form = DocumentForm(self.user, request.POST, request.FILES)
-        form.is_valid()
-        form.save()
-        self.assertEqual(len(Document.objects.all()), 2)
-        doc1, doc2 = Document.objects.all()
-        self.assertEqual(doc1.blob.read(), "Bring us a shrubbery!!")
-        self.assertEqual(doc2.blob.read(), "Bring us another shrubbery!!")
+from forms import *
 
 class DocumentModelTest(TestCase):
     fixtures = ['corpus']
@@ -177,7 +94,6 @@ class DocumentModelTest(TestCase):
         doc.date_uploaded = datetime.now()
         doc.save()
         self.assertEqual(doc.slug, "42.txt")
-
 
 def _create_document(filename, contents, owner):
     fp = StringIO()
