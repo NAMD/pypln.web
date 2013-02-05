NAMD/pypln.web

commit 2dea4fab3702060bf0daf71e27271517f0f81692

Fixes KeyError in wordcloud visualization for Unknown languages
When trying to filter stopwords the visualization would not find an empty key
in the dictionary, raising a KeyError

pypln/web/apps/core/visualizations.py
@@ -77,8 +77,8 @@ def _statistics(data):
 
 def _wordcloud(data):
     stopwords_list = list(punctuation)
-    document_language = LANGUAGES[data['language']].lower()
-    if document_language in stopwords.fileids():
+    document_language = LANGUAGES.get(data['language'])
+    if document_language and document_language.lower() in stopwords.fileids():
         stopwords_list += stopwords.words(document_language)
     data['freqdist'] = [[x[0], x[1]] for x in data['freqdist'] \
                                                  if x[0] not in stopwords_list]

commit eec7f12cbf124e75a55538e5412000c53aab2250

Fixes bug introduced in 2dea4fab370

pypln/web/apps/core/visualizations.py
@@ -43,7 +43,7 @@ def _wordcloud(data):
     stopwords_list = list(punctuation)
     document_language = LANGUAGES.get(data['language'])
     if document_language and document_language.lower() in stopwords.fileids():
-        stopwords_list += stopwords.words(document_language)
+        stopwords_list += stopwords.words(document_language.lower())
     data['freqdist'] = [[x[0], x[1]] for x in data['freqdist'] \
                                                  if x[0] not in stopwords_list]
     return data