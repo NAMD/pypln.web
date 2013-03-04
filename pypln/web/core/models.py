674d0ae7230c46d14c6cc4c4e19fe282e32d331f
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-03-04 12:12:35 -0300

    Adds basic Corpus and Document models

---
+++ pypln/web/core/models.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright 2012 NAMD-EMAP-FGV
+#
+# This file is part of PyPLN. You can get more information at: http://pypln.org/.
+#
+# PyPLN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyPLN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+from django.db import models
+
+
+class Corpus(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User')
+
+    class Meta:
+        verbose_name_plural = 'corpora'
+
+    def __unicode__(self):
+        return self.name
+
+
+class Document(models.Model):
+    blob = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User')
+    corpora = models.ForeignKey(Corpus)
+
+    def __unicode__(self):
+        return self.blob.name
--- pypln/web/settings.py
+++ pypln/web/settings.py
@@ -69,12 +69,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -140,6 +140,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+
+    'pypln.web.core',
 )
 
 # A sample logging configuration. The only tangible logging
