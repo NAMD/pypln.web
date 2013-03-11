4961bad85bb879b664e91476f31a31c8be32d8fd
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-03-11 15:43:47 -0300

    Sets the mongodb up for tests in TestWithMongo

--- pypln/web/core/tests/utils.py
+++ pypln/web/core/tests/utils.py
@@ -16,9 +16,14 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+import json
+import os
 
+from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase
+
+from mongodict import MongoDict
 
 from pypln.web.core.models import Document, gridfs_storage
 
@@ -38,6 +43,16 @@ class TestWithMongo(TestCase):
         for doc in Document.objects.all():
             gridfs_storage.save(os.path.basename(doc.blob.name),
                     "This is a test file with some test text.")
+
+        self.store = MongoDict(host=settings.MONGODB_CONFIG['host'],
+               port=settings.MONGODB_CONFIG['port'],
+               database=settings.MONGODB_CONFIG['database'],
+               collection=settings.MONGODB_CONFIG['analysis_collection'])
+
+        filename = os.path.join(settings.PROJECT_ROOT, 'core/fixtures/mongodb/analysis.json')
+        with open(filename, 'r') as mongo_fixture:
+            for obj in json.load(mongo_fixture):
+                self.store._collection.insert(obj)
 
     def _post_teardown(self, *args, **kwargs):
         gridfs_storage._connection.drop_database(gridfs_storage.database)
