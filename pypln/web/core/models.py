ceb1f1caf37965bbb0f9c2bcaf972d3ab577f76b
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-03-12 14:21:52 -0300

    Adds a `StoreProxy` to the `Document`, so you can access the data in the backend
    This is supposed to make it easier to get analysis data for a given `Document`.
    It also concentrates the code that is dependent on the store implementation in
    one place, that should be easily modified if necessary.

--- pypln/web/core/models.py
+++ pypln/web/core/models.py
@@ -16,8 +16,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+from UserDict import DictMixin
+
 from django.conf import settings
 from django.db import models
+
+from mongodict import MongoDict
 
 from pypln.web.core.storage import GridFSStorage
 
@@ -42,6 +46,24 @@ class Corpus(models.Model):
     def __unicode__(self):
         return self.name
 
+class StoreProxy(DictMixin):
+    def __init__(self, document_id, store):
+        self._store = store
+        self.document_id = document_id
+
+    def __getitem__(self, key):
+        try:
+            return self._store['id:{}:{}'.format(self.document_id, key)]
+        except KeyError:
+            if key == "_properties":
+                msg = "Can't find information for document with id {document_id}"
+            else:
+                msg = "Can't find key {key} for document with id {document_id}"
+            raise KeyError(msg.format(key=key, document_id=self.document_id))
+
+    def keys(self, *args, **kwargs):
+        return self['_properties']
+
 
 class Document(models.Model):
     blob = models.FileField(upload_to='/', storage=gridfs_storage)
@@ -49,5 +71,19 @@ class Document(models.Model):
     owner = models.ForeignKey('auth.User')
     corpus = models.ForeignKey(Corpus)
 
+    _store = None
+
     def __unicode__(self):
         return self.blob.name
+
+    @property
+    def properties(self):
+        if self.id is None:
+            raise ValueError("This document was not saved, so you cannot "
+                    "retrieve it's information from the backend.")
+        if self._store is None:
+            self._store = MongoDict(host=settings.MONGODB_CONFIG['host'],
+                   port=settings.MONGODB_CONFIG['port'],
+                   database=settings.MONGODB_CONFIG['database'],
+                   collection=settings.MONGODB_CONFIG['analysis_collection'])
+        return StoreProxy(self.id, self._store)
