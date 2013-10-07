6cd7606684572fa2065077bbe1c175102c92b774
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-10-07 11:31:08 -0300

    Fixes SuspiciousOperation filter

--- pypln/web/settings/production.py
+++ pypln/web/settings/production.py
@@ -86,8 +86,8 @@ from django.core.exceptions import SuspiciousOperation
 
 def skip_suspicious_operations(record):
     if record.exc_info:
-        exc_value = record.exc_info[0]
-        if isinstance(exc_value, SuspiciousOperation):
+        exc = record.exc_info[1]
+        if isinstance(exc, SuspiciousOperation):
             return False
     return True
 
