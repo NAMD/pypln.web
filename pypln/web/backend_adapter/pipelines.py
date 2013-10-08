a96d0d9f74b720cd7a45c1eab0102075488b451b
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-10-08 16:06:32 -0300

    Removes Stanford NER from the default pipeline

--- pypln/web/backend_adapter/pipelines.py
+++ pypln/web/backend_adapter/pipelines.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from pypelinin import Job, Pipeline, PipelineManager, Client
 
 default_pipeline = {
-    Job("Extractor"): (Job("StanfordNER"), Job("Tokenizer")),
+    Job("Extractor"): Job("Tokenizer"),
     Job("Tokenizer"): (Job("POS"), Job("FreqDist")),
     Job("FreqDist"): Job("Statistics")
 }
