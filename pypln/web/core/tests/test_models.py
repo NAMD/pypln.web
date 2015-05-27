898f8f8c5826dcdf7e681f87b7fac3d2815f83b1
Flavio Amieiro <amieiro.flavio@gmail.com>
2015-05-27 06:02:48 +0200

    Updates list of expected properties for a fully analysed document

--- pypln/web/core/tests/test_models.py
+++ pypln/web/core/tests/test_models.py
@@ -52,10 +52,11 @@ class DocumentModelTest(TestWithMongo):
     fixtures = ['users', 'corpora', 'documents']
 
     def test_get_properties_from_store(self):
-        expected_data = ["mimetype", "freqdist", "average_sentence_repertoire",
-            "language", "average_sentence_length", "sentences", "momentum_1",
-            "pos", "momentum_3", "file_metadata", "tokens", "repertoire",
-            "text", "tagset", "momentum_4", "momentum_2"]
+        expected_data = ["average_sentence_length", "average_sentence_repertoire",
+            "contents", "file_id", "file_metadata", "filename", "forced_decoding",
+            "freqdist", "language", "length", "md5", "mimetype", "momentum_1",
+            "momentum_2", "momentum_3", "momentum_4", "palavras_raw_ran", "pos",
+            "repertoire", "sentences", "tagset", "text", "tokens", "upload_date"]
         document = Document.objects.all()[0]
         self.assertEqual(document.properties.keys(), expected_data)
 
