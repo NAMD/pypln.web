b628d8042db5325ee581d68ff7e7deaca9df3809
Eduardo Stalinho <eduardooc.86@gmail.com>
2016-01-27 18:41:24 -0200

    move settings to only one file

--- pypln/web/settings.py
+++ pypln/web/settings.py
@@ -18,98 +18,96 @@
 # along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
 import os
 
+from decouple import config, Csv
+from dj_database_url import parse as db_url
+
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
+
+
+def parse_url(url):
+    urlparse.uses_netloc.append('mongodb')
+    url = urlparse.urlparse(url)
+
+    path = url.path[1:]
+    path = path.split('?', 2)[0]
+
+    return {
+        'database': path or '',
+        'port': url.port or '',
+        'host': url.p or '',
+    }
+
+    return config
+
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__),
     os.path.pardir))
 
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=Csv())
 TEMPLATE_DEBUG = DEBUG
 
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+
+if EMAIL_BACKEND != 'django.core.mail.backends.console.EmailBackend':
+    EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = config('EMAIL_CONFIG', default='example_host,25,username,password', cast=Csv())
+
+
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    config('ADMIN', cast=Csv())
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, 'dev.db'),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
+    'default': config('DATABASE_URL', default='sqlite://dev.db', cast=db_url)
 }
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+MONGODB_CONFIG = config('MONGODB_CONFIG', default='mongodb://localhost:27017/pypln', cast=parse_url)
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
+
+MONGODB_CONFIG.update({
+    'analysis_collection': config('ANALYSIS_COLLECTION', default='analysis'),
+    'gridfs_collection': config('GRIDFS_COLLECTION', default='files'),
+    'monitoring_collection': config('MONITORING_COLLECTION', default='monitoring'),
+})
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 TIME_ZONE = 'America/Chicago'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
 
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
 STATIC_ROOT = ''
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'louzsg2)(k8smz_jojk$y99nc-p11kia1unl9+yvvdvpel!z1b'
+SECRET_KEY = config('SECRET_KEY', default='louzsg2)(k8smz_jojk$y99nc-p11kia1unl9+yvvdvpel!z1b')
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -118,8 +116,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -139,9 +135,6 @@ ROOT_URLCONF = 'pypln.web.urls'
 WSGI_APPLICATION = 'pypln.web.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_ROOT, "templates"),
 )
 
@@ -162,11 +155,6 @@ INSTALLED_APPS = (
     'pypln.web.backend_adapter',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -205,5 +193,28 @@ REST_FRAMEWORK = {
 
 
 ELASTICSEARCH_CONFIG = {
-    'hosts': ['127.0.0.1'],
+    'hosts': config('ELASTICSEARCH_HOSTS', cast=Csv()),
 }
+
+SERVE_MEDIA = config('SERVE_MEDIA', default=False, cast=bool)
+
+ROUTER_API = config('ROUTER_API')
+ROUTER_BROADCAST = config('ROUTER_BROADCAST')
+ROUTER_TIMEOUT = 5
+
+if not settings.DEBUG:
+    from django.core.exceptions import SuspiciousOperation
+
+    def skip_suspicious_operations(record):
+        if record.exc_info:
+            exc = record.exc_info[1]
+            if isinstance(exc, SuspiciousOperation):
+                return False
+        return True
+
+    LOGGING['filters']['skip_suspicious_operations'] = {
+        '()': 'django.utils.log.CallbackFilter',
+        'callback': skip_suspicious_operations,
+    }
+
+    LOGGING['handlers']['mail_admins']['filters'].append('skip_suspicious_operations')
